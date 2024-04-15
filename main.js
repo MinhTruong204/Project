cbfb3beb7e301054dc9cb9c7d1f1e4ea2b9526d6
MinhTruong204 <truongocminh204@gmail.com>
2024-04-15 10:59:40 +0700

    Commit 4 (school

---
+++ main.js
@@ -0,0 +1,13 @@
+let sectionElement = document.querySelectorAll(".section-item");
+let activeSectionElement ;
+
+sectionElement.forEach(function(item){
+    item.addEventListener("click", function(){
+        activeSectionElement = document.querySelector(".active-section");
+        activeSectionElement.classList.remove("active-section");
+        activeSectionElement.classList.add("unactive-section");
+        item.classList.remove("unactive-section")
+        item.classList.add("active-section");
+    }
+);})
+
